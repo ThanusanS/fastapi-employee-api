40bd9e2200e577dcf09899b5ed47d206b3b5b46b
ThanusanS <thanusan0411@gmail.com>
2026-02-24 16:13:13 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+
+
+
+@app.get("/")
+def home():
+    return {"massage":"mmnnnkkkli"}
+
+
+
+@app.get("/display/{idn}")
+def show_id(idn):
+    return f"myId: {idn}"
+
+
+
+
+
+
+
+
+emp=[
+
+    {"id":1234,"name":"vinoth","place":"Tamilnadu"},
+    {"id":5678,"name":"Kamal","place":"Canada"}
+]
+
+
+@app.get("/dis/{idmy}")
+def view(idmy:int):
+    for details in emp:
+        if details['id']==idmy:
+            return details
+
+
+
+
+
+
+
+
+
+
+
+
